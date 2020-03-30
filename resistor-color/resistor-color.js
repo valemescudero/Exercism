e753838c2f1c2ef5c74959b551eca405016990ce
Evadne Meraki <evadne.meraki@gmail.com>
2020-03-30 18:44:37 -0300

    Resistor-color

---
+++ resistor-color/resistor-color.js
@@ -0,0 +1,13 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+
+export const COLORS = ['black','brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
+
+export const colorCode = (color) => {
+let index = COLORS.indexOf(color);
+
+return index;
+};
