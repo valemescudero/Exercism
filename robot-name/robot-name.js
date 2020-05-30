938c98d6ebeba9af8f3f184b2de48ddf7394d171
Evadne Meraki <evadne.meraki@gmail.com>
2020-05-29 21:59:38 -0300

    Robot-name

--- robot-name/robot-name.js
+++ robot-name/robot-name.js
@@ -1,6 +1,43 @@
 // This is only a SKELETON file for the 'Robot Name' exercise. It's been
 // provided as a convenience to get your started writing code faster.
 
-export class Robot { }
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+export class Robot { 
+    constructor() {
+    this.usedNames = [];
+    this.nameAux = this.releaseNames();
+    }
+    randomizeLetter() {
+        return alphabet[Math.floor(Math.random() * 26)];
+    }
+    randomizeNumber() {
+        return Math.floor(Math.random() * 10).toString();
+    }
+    generateName() {
+        let aux = [];
+        for (let i=0; i<2 ; i++){
+            aux.push(this.randomizeLetter());
+        }
+        for (let i=0; i<3 ; i++){
+            aux.push(this.randomizeNumber());
+        }
+        return aux.join('');
+    }    
+    releaseNames() {        
+        let aux = '';
+        do {
+           aux = this.generateName();
+        } while (this.usedNames.indexOf(aux) !== -1)
+        this.usedNames.push(aux);
+        return aux;
+    }
+    reset() {
+        this.nameAux = this.releaseNames();
+    }
+    get name() {
+    return this.nameAux;
+    }
+}
 
 Robot.releaseNames = () => { };
